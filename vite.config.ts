65b5c87141fe107445327431e0314d1c21456861
HansHeidmann <hansheidmann@gmail.com>
2025-02-11 18:23:03 -0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    headers: {
+        "Content-Security-Policy": "script-src 'self' 'unsafe-eval'"
+    },
     port: 8080,
     open: true,
     watch: {
         usePolling: true
     }
-  },
+  }
+    
 });
