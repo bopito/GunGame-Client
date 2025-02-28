a9584134faae51fce42292f2a09724f293721f21
SeokhyunRew <darkknightttt@naver.com>
2025-02-28 09:03:21 +0900

    Feat: edit,add code for equip weapon

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import * as BABYLON from "@babylonjs/core";
 import "@babylonjs/loaders";
 import { GameGUI } from "./gui.ts";
-import { Player } from "./player.ts";
+import player, { Player } from "./player.ts";
+import { Weapon } from "./weapon.ts";
 
 document.addEventListener("DOMContentLoaded", async () => {
 
@@ -54,7 +55,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
     light.intensity = 0.8;
 
-    // Create Ground 
+    // Create Ground
     const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 100, height: 100 }, scene);
     const groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
     groundMaterial.diffuseTexture = new BABYLON.Texture(`${assetBasePath}assets/grass_background.png`, scene);
@@ -88,11 +89,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     connectWebSocket();
 
     // Handle WebSocket Messages
-    
-
-    
-
-    // Handle WebSocket Messages
     function handleWebSocketMessage(event: MessageEvent): void {
         const data = JSON.parse(event.data);
 
@@ -102,25 +98,18 @@ document.addEventListener("DOMContentLoaded", async () => {
             console.log(`Assigned Player ID: ${playerId}`);
         }
 
-        // Remove Disconnected Players
-        // if (data.type === "player_removed") {
-        //     const disconnectedPlayerID = data.playerID;
-        //     delete localPlayers.disconnectedPlayerID;
-        //     console.log(`Removed Player: ${playerId}`);
-        // }
-
         // Step 2: Process all players
-        if (data.entities) {
-            const serverPlayers = Object.values(data.entities.players) as Player[];
+        if (data.type === "broadcast") {
+            const serverPlayers = Object.values(data.entities.players) as player[];
             const serverPlayerIds = new Set(serverPlayers.map(player => player.id));
-            
+
             for (const playerData of serverPlayers) {
                 // Remove disconnected players
                 for (const id in localPlayers) {
                     if (!serverPlayerIds.has(id)) {
                         console.log(`Removing player not in server state: ${id}`);
-                        if(localPlayers[id]) {
-                            localPlayers[id].mesh!.dispose(); // Remove from Babylon.js
+                        if (localPlayers[id]) {
+                            localPlayers[id].mesh!.dispose();
                             delete localPlayers[id];
                             gui.removePlayer(id);
                         }
@@ -130,34 +119,68 @@ document.addEventListener("DOMContentLoaded", async () => {
                 // Add New players
                 if (!localPlayers[playerData.id]) {
                     console.log(`Creating new player: ${playerData.id}`);
-                    // Instantiate an instance of Player
-                    const newPlayer = playerData;
-                    
-                    // Create 3D object
+                    // create new weapon object from server data
+                    const newWeapon = new Weapon(
+                        playerData.currentWeapon.id,
+                        playerData.currentWeapon.name,
+                        playerData.currentWeapon.damage,
+                        playerData.currentWeapon.bulletSpeed,
+                        playerData.currentWeapon.range,
+                        playerData.currentWeapon.reloadTime,
+                        playerData.currentWeapon.rateOfFire,
+                        playerData.currentWeapon.maxAmmo,
+                        playerData.currentWeapon.currentAmmo,
+                        playerData.currentWeapon.reserveAmmo
+                    );
+
+                    const newPlayer = new Player(
+                        playerData.id,
+                        playerData.x,
+                        playerData.y,
+                        playerData.z,
+                        playerData.angle,
+                        playerData.team,
+                        playerData.health,
+                        playerData.score,
+                        newWeapon
+                    );
+
                     const newPlayerMesh = BABYLON.MeshBuilder.CreateBox(`player_${playerData.id}`, { size: 2 }, scene);
                     newPlayerMesh.position.set(playerData.x, 1, -playerData.z);
                     newPlayer.mesh = newPlayerMesh;
+
+                    newPlayer.currentWeapon.attachToPlayer(scene, newPlayer.mesh);
                     localPlayers[playerData.id] = newPlayer;
+                } else {
+                    let player = localPlayers[playerData.id];
+                    player.updateData(playerData.x, playerData.y, playerData.z, playerData.angle, playerData.health, playerData.score);
 
-                    console.log(newPlayer);
-                    
+                    // update weapon data
+                    player.updateWeaponAmmo(
+                        playerData.currentWeapon.currentAmmo,
+                        playerData.currentWeapon.reserveAmmo
+                    );
+                    player.updateWeapon(
+                        new Weapon(
+                            playerData.currentWeapon.id,
+                            playerData.currentWeapon.name,
+                            playerData.currentWeapon.damage,
+                            playerData.currentWeapon.bulletSpeed,
+                            playerData.currentWeapon.range,
+                            playerData.currentWeapon.reloadTime,
+                            playerData.currentWeapon.rateOfFire,
+                            playerData.currentWeapon.maxAmmo,
+                            playerData.currentWeapon.currentAmmo,
+                            playerData.currentWeapon.reserveAmmo,
+                        ),
+                        scene
+                    );
+
+                    if (player.mesh) {
+                        player.mesh.position.set(playerData.x, 1, -playerData.z);
+                        player.mesh.rotation.y = playerData.angle;
+                    }
                 }
-
-                // Update position & rotation
-                localPlayers[playerData.id].x = playerData.x;
-                localPlayers[playerData.id].z = playerData.z;
-                localPlayers[playerData.id].angle = playerData.angle;
-                localPlayers[playerData.id].health = playerData.health;
-                localPlayers[playerData.id].score = playerData.score;
-
-                // Update mesh position
-                if (localPlayers[playerData.id].mesh) {
-                    localPlayers[playerData.id].mesh!.position.set(playerData.x, 1, -playerData.z);
-                    localPlayers[playerData.id].mesh!.rotation.y = playerData.angle;
-                }
-
-                // Update GUI Player List
-                gui.updatePlayer(playerData);
             }
 
             // Only update camera if the player exists
@@ -167,13 +190,12 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
     }
 
-    
-    //
-    // Handle Player Input
-    //
+    // Handle player inputs
     const keys: Record<string, boolean> = {};
     scene.onKeyboardObservable.add((kbInfo) => {
         const key = kbInfo.event.key.toLowerCase();
+
+        // Movement keys
         if (["a", "w", "s", "d"].includes(key)) {
             if (kbInfo.type === BABYLON.KeyboardEventTypes.KEYDOWN) {
                 keys[key] = true;
@@ -199,50 +221,50 @@ document.addEventListener("DOMContentLoaded", async () => {
     //
     scene.onPointerMove = (evt) => {
         //console.log("Pointer moved:", evt.clientX, evt.clientY);
-    
+
         if (!localPlayers[playerId]) {
             //console.warn("Player data not found for local player:", playerId);
             return;
         }
-    
+
         // Manually cast a ray
         const pickResult = scene.pick(scene.pointerX, scene.pointerY);
-    
+
         if (pickResult?.hit) {
             //console.log("Raycast hit:", pickResult.pickedMesh?.name);
-    
+
             const player = localPlayers[playerId];
             const mousePosition = pickResult.pickedPoint;
-    
+
             if (mousePosition) {
                 const playerPos = player.mesh!.position;
-    
+
                 // Calculate angle between player and cursor
                 const dx = mousePosition.x - playerPos.x;
                 const dz = mousePosition.z - playerPos.z;
-                const targetAngle = Math.atan2(dx, dz); 
-    
+                const targetAngle = Math.atan2(dx, dz);
+
                 // Rotate player smoothly
                 player.mesh!.rotation.y = targetAngle;
-    
+
                 sendRotationUpdate(targetAngle);
             }
         } else {
             //console.warn("Raycast did NOT hit any object.");
         }
     };
-    
+
     //
     // Send Player y-Rotation to Server
     //
     function sendRotationUpdate(angle: number) {
         if (!isWebSocketConnected || playerId === "undefined") return;
-        socket.send(JSON.stringify({ 
-            action: "rotate", 
+        socket.send(JSON.stringify({
+            action: "rotate",
             angle: angle
         }));
     }
-    
+
     //
     // Capture Mouse-Click Event in Scene
     //
@@ -256,7 +278,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
         // Right Click
         if(event.button == 2){
-                
+
         }
     }
 
@@ -265,21 +287,21 @@ document.addEventListener("DOMContentLoaded", async () => {
     //
     function sendMouseUpdate(angle: number) {
         if (!isWebSocketConnected || playerId === "undefined") return;
-        socket.send(JSON.stringify({ 
-            action: "mouseEvent", 
+        socket.send(JSON.stringify({
+            action: "mouseEvent",
             angle: angle
         }));
     }
 
     function updateCamera(playerMesh: BABYLON.Mesh): void {
         if (!playerMesh) return;
-    
+
         camera.position.x += (playerMesh.position.x - camera.position.x); // * 0.9; // smooth
         camera.position.z += (playerMesh.position.z - camera.position.z - 40);
         camera.position.y = 40;
         camera.rotation.set(Math.PI / 4, 0, 0);
     }
-    
+
 
     engine.runRenderLoop(() => {
         try {
